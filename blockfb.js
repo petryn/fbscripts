c1144f86eb268f9dd34a85bb8e60c8215eb3be08
Petrynski <Piotr.Petrynski@lionbridge.com>
2020-08-07 16:09:58 +0200

    updated to v2.0.0

--- blockfb.js
+++ blockfb.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name     block FB sponsored posts and propositions
-// @version  1.1.0
+// @name     block FB sponsored posts
+// @version  2.0.0
 // @grant    none
 // @match    https://www.facebook.com/*
 // ==/UserScript==
@@ -9,40 +9,45 @@ function hideAds() {
     var fullText;
 
     function loopNodes(node) {
-        if (node.dataset != undefined && node.dataset.content != undefined && !isHidden(node)) {
-            fullText += node.dataset.content;
+        if (node.nodeName === "#text" && node.parentNode.offsetTop === 0) { 
+            fullText += node.wholeText;
         } else {
             node.childNodes.forEach((child) => {
                 loopNodes(child);
             });
-        }
-
+        }     
     }
-
-    function isHidden(el) {
-        return (el.offsetParent === null)
-    }
-
-    document.querySelectorAll("[id^='fbfeed_sub__header_id']").forEach((el) => {
+    
+    document.querySelectorAll("div[id^=hyperfeed_story_id]").forEach((parent_div) =>{  
+      if(parent_div != null){
+        var title_el = parent_div.querySelector("div[id^='fbfeed_s_header']>span:nth-child(3)>span>a>span, div[id^='fbfeed_s_header']>span:nth-child(3)>span>span>a");
+    
         fullText = "";
-        loopNodes(el);
-        if (fullText == 'Sponsorowane') {
-            console.log(el.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode);
-            el.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.innerHTML = "Hidden sponsored"
+
+        if(title_el != null){
+          loopNodes(title_el);
+        }        
+        
+        console.log(fullText);
+        console.log(parent_div);
+        
+        if (fullText == 'Sponsorowane') {          
+          parent_div.innerHTML = "Hidden!";
         }
-    });
-  
-    document.querySelectorAll(".x_9y3d1sxtq").forEach((el) => {
-        if(el.textContent == "Propozycje dla Ciebie"){
-          el.parentNode.parentNode.parentNode.parentNode.innerHTML = "Hidden proposition";
-        }
-    });
+      }
+    });   
 }
 
-setTimeout(hideAds, 1000);
+function debounce(method, delay) {
+    clearTimeout(method._tId);
+    method._tId= setTimeout(function(){
+        method();
+    }, delay);
+}
+
 (function() {
-    window.addEventListener('scroll', () => {
-        hideAds();
-        setTimeout(hideAds, 1000);
+    window.addEventListener('scroll', () => {   
+      debounce(hideAds, 1000);
     });
 })();
+
